eab50c682d681a7fa0c43f78b32042a15ec43556
Alex Xavier <alexjr.123@hotmail.com>
2023-03-22 21:59:48 -0300

    Variáveis do JS

---
+++ index.js
@@ -0,0 +1,15 @@
+/** Variáveis numéricas */
+var $number = 10;
+var $number2 = 5.5;
+
+console.log(typeof $number, $number2);
+
+/** Variáveis de cadeia de caracteres (String) */
+var $string = 'Olá mundo';
+$string = "hello world"; 
+// sou um comentário
+console.log(typeof $string, $string);
+
+//Variáveis boleanas (Verdadeiro = true ou Falso = false)
+var $boleanas = true;
+console.log(typeof $boleanas,$boleanas);
